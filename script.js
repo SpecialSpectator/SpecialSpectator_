c2e07a8060a7ae69a24f293b4e069bd9a575ccba
SpecialSpectator <sandveceextreme@gmail.com>
2026-02-20 13:41:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,33 +13,11 @@ var _0x53a645 = _0x28d8;
     }
 }(_0xf0f6, 0xcb047), window[_0x53a645(0x225)] = [], window['started'] = ![], window['start'] = () => {
     var _0x327871 = _0x53a645;
-    window[_0x327871(0x1f7)] = !![];
-    
-    function startBots(count) {
-        var _0x394253 = document[_0x327871(0x1ad)](_0x327871(0x1d9))['value'];
-        for (let _0xed40a7 = 0x0; _0xed40a7 < count; _0xed40a7++) {
-            setTimeout(() => {
-                var _0x1321b6 = _0x327871;
-                grecaptcha[_0x1321b6(0x196)](function () {
-                    var _0x224c69 = _0x1321b6;
-                    grecaptcha[_0x224c69(0x172)](_0x224c69(0x310), {
-                        'action': _0x224c69(0x321)
-                    })[_0x224c69(0x277)](function (_0x48abc0) {
-                        var _0x3c130d = _0x224c69;
-                        let botIndex = window[_0x3c130d(0x225)].length;
-                        window[_0x3c130d(0x225)][_0x3c130d(0x282)](new _0x22f999(_0x3c130d(0x2c8) + _0x394253, _0x48abc0, botIndex));
-                    });
-                });
-            }, 0x1f4 * _0xed40a7);
-        }
-    }
-    
+    window[_0x327871(0x1f7)] = !![], window['count'] = 0x1;
     class _0x22f999 {
-        constructor(_0x53f296, _0x3cfa09, _0xbotIndex) {
+        constructor(_0x53f296, _0x3cfa09) {
             var _0x3f7202 = _0x327871;
-            this['token'] = _0x3cfa09;
-            this['botIndex'] = _0xbotIndex;
-            this[_0x3f7202(0x27a)](_0x53f296);
+            this['token'] = _0x3cfa09, this[_0x3f7202(0x27a)](_0x53f296);
         } [_0x327871(0x27a)](_0x12448d) {
             var _0x337a80 = _0x327871;
             this[_0x337a80(0x263)] = !![], this[_0x337a80(0x2ec)] = _0x12448d, this['ws'] = new WebSocket(_0x12448d), this['ws'][_0x337a80(0x261)] = _0x337a80(0x205), this['ws']['onmessage'] = this[_0x337a80(0x290)][_0x337a80(0x2ab)](this), this['ws']['onopen'] = this[_0x337a80(0x1aa)]['bind'](this), this['ws'][_0x337a80(0x2f1)] = this[_0x337a80(0x19e)][_0x337a80(0x2ab)](this), this['ws'][_0x337a80(0x215)] = this['onError']['bind'](this);
@@ -78,9 +56,10 @@ var _0x53a645 = _0x28d8;
                 _0x376158['setUint8'](0x0, 0x5a), _0x376158[_0x5058a6(0x243)](0x1, 0x75bcd15, !![]), this[_0x5058a6(0x20a)](_0x376158);
             }, 0x3e8), setTimeout(async () => {
                 var _0x1497ba = _0x1959b5;
-                for (let _0x37acf5 = 0x0; _0x37acf5 <= this['botIndex']; _0x37acf5++) {
+                for (let _0x37acf5 = 0x0; _0x37acf5 < window[_0x1497ba(0x1c3)]; _0x37acf5++) {
                     this[_0x1497ba(0x1a7)](), await this['sleep'](0x64);
                 };
+                window[_0x1497ba(0x1c3)]++;
             }, 0x7d0);
         } ['onClose'](_0x4cca7d) {
             var _0x3f3e30 = _0x327871;
@@ -100,24 +79,49 @@ var _0x53a645 = _0x28d8;
             this[_0x2fdf99(0x219)] && this['ws'][_0x2fdf99(0x20a)](_0x3c591f['buffer']);
         }
     }
+    var _0x394253 = document[_0x327871(0x1ad)](_0x327871(0x1d9))['value'];
+    localStorage[_0x327871(0x24b)]('gameMode', _0x394253);
     
-    startBots(0x4);
+    // Bot başlatma fonksiyonu
+    function launchBots(botSayisi) {
+        for (let _0xed40a7 = 0x0; _0xed40a7 < botSayisi; _0xed40a7++) {
+            setTimeout(() => {
+                var _0x1321b6 = _0x327871;
+                grecaptcha[_0x1321b6(0x196)](function () {
+                    var _0x224c69 = _0x1321b6;
+                    grecaptcha[_0x224c69(0x172)](_0x224c69(0x310), {
+                        'action': _0x224c69(0x321)
+                    })[_0x224c69(0x277)](function (_0x48abc0) {
+                        var _0x3c130d = _0x224c69;
+                        window[_0x3c130d(0x225)][_0x3c130d(0x282)](new _0x22f999(_0x3c130d(0x2c8) + _0x394253, _0x48abc0));
+                    });
+                });
+            }, 0x1f4 * _0xed40a7);
+        }
+    }
+    
+    // İlk 4 botu başlat
+    launchBots(4);
     
 }, document[_0x53a645(0x30e)]('keydown', function (_0x509101) {
     var _0x569dfa = _0x53a645;
+    
+    // " tuşu
     if (_0x509101['key'] === '\"') {
         if (window[_0x569dfa(0x1f7)] === !![]) return;
         window['start']();
     }
-    if (_0x509101['key'] === 'ç' || _0x509101['key'] === 'Ç' || _0x509101['code'] === 'KeyC' && _0x509101['ctrlKey']) {
+    
+    // ç tuşu - ekstra 4 bot
+    if (_0x509101['key'] === 'ç' || _0x509101['key'] === 'Ç') {
         if (window['started'] === false) {
             window['start']();
         } else {
+            // Mevcut botların yanına 4 tane daha ekle
             var _0x327871 = _0x569dfa;
             var _0x394253 = document[_0x327871(0x1ad)](_0x327871(0x1d9))['value'];
             
-            let currentBotCount = window[_0x327871(0x225)].length;
-            
+            // 4 yeni bot başlat
             for (let i = 0; i < 4; i++) {
                 setTimeout(() => {
                     var _0x1321b6 = _0x327871;
@@ -127,15 +131,22 @@ var _0x53a645 = _0x28d8;
                             'action': _0x224c69(0x321)
                         })[_0x224c69(0x277)](function (_0x48abc0) {
                             var _0x3c130d = _0x224c69;
-                            let botIndex = window[_0x3c130d(0x225)].length;
-                            window[_0x3c130d(0x225)][_0x3c130d(0x282)](new _0x22f999(_0x3c130d(0x2c8) + _0x394253, _0x48abc0, botIndex));
+                            window[_0x3c130d(0x225)][_0x3c130d(0x282)](new _0x22f999(_0x3c130d(0x2c8) + _0x394253, _0x48abc0));
                         });
                     });
                 }, i * 500);
             }
         }
     }
-    _0x509101[_0x569dfa(0x226)] === 'b' && (document['querySelector'](_0x569dfa(0x26c))[_0x569dfa(0x32e)][_0x569dfa(0x224)] = _0x569dfa(0x227), window['xa']()), _0x509101['key'] === _0x569dfa(0x1f9) && (document[_0x569dfa(0x2a5)](_0x569dfa(0x26c))['style'][_0x569dfa(0x224)] = '');
+    
+    if (_0x509101['key'] === 'b') {
+        document.querySelector("#main-login-section").style.display = "none";
+        window.xa();
+    }
+    
+    if (_0x509101['key'] === 'Escape') {
+        document.querySelector("#main-login-section").style.display = "";
+    }
 }));
 
 function _0xf0f6() {
